a82b4abcf0139721095487add0d2765f5eae5644
restoker <milthon.doom@gmail.com>
2026-06-12 07:59:17 -0500

    pdf fixed

--- components/PlanTrabajo.tsx
+++ components/PlanTrabajo.tsx
@@ -1,37 +1,8 @@
 'use client';
 
-import { createPluginRegistration } from '@embedpdf/core';
-import { EmbedPDF } from '@embedpdf/core/react';
 import { usePdfiumEngine } from '@embedpdf/engines/react';
-// import { useZoom } from '@embedpdf/plugin-zoom/react';
-
-// Import the essential plugins
-import { Viewport, ViewportPluginPackage } from '@embedpdf/plugin-viewport/react';
-import { Scroller, ScrollPluginPackage } from '@embedpdf/plugin-scroll/react';
-import {
-    DocumentContent,
-    DocumentManagerPluginPackage,
-} from '@embedpdf/plugin-document-manager/react';
-import { RenderLayer, RenderPluginPackage } from '@embedpdf/plugin-render/react';
-import { ZoomPluginPackage, ZoomMode } from '@embedpdf/plugin-zoom/react';
-// import { ZoomToolbar } from './ZoomToolbar';
 import { PDFViewer } from '@embedpdf/react-pdf-viewer';
-// 1. Register the plugins you need
-const plugins = [
-    createPluginRegistration(DocumentManagerPluginPackage, {
-        initialDocuments: [{ url: '/img/ULTIMO.pdf' }],
-    }),
-    createPluginRegistration(ViewportPluginPackage),
-    createPluginRegistration(ScrollPluginPackage),
-    createPluginRegistration(RenderPluginPackage),
-    createPluginRegistration(ZoomPluginPackage, {
-        defaultZoomLevel: ZoomMode.FitPage, // You can pass options here!
-    }),
-];
 
-// import dynamic from 'next/dynamic';
-
-// Link the canvas worker to a CDN source for processing
 const PlanTrabajo = ({ fileUrl = '/img/PLAN-DE-TRABAJO-VLADI-ARANYA.pdf' }: { fileUrl?: string }) => {
     const { engine, isLoading } = usePdfiumEngine();
 
@@ -42,13 +13,14 @@ const PlanTrabajo = ({ fileUrl = '/img/PLAN-DE-TRABAJO-VLADI-ARANYA.pdf' }: { fi
     return (
         <div className='relative h-dvh overflow-hidden'>
             {/* <ZoomToolbar documentId={fileUrl} /> */}
-            <PDFViewer
+            {/* <PDFViewer
                 className='h-125'
                 config={{
                     src: fileUrl,
                     // theme: { preference: 'light' }
                 }}
-            />
+            /> */}
+            <iframe src={fileUrl} className='h-125' style={{ width: '100%', border: 'none' }}></iframe>
         </div>
     );
 };
